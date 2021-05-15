61c5026b5a696d179886081b390ce4d5575ebc49
skyun <skyun@a2tec.co.kr>
2021-05-15 20:13:57 +0900

    init source

---
+++ src/main/java/com/kenux/datetimetest/DatetimetestApplication.java
@@ -0,0 +1,13 @@
+package com.kenux.datetimetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatetimetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatetimetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenux/datetimetest/DatetimetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenux.datetimetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatetimetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
